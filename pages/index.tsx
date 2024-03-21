34bb118cdada66894fcb09cc66de917ae364e3ff
Edson Alencar <edson.cesar.alencar@gmail.com>
2024-03-20 23:37:22 -0300

    fix.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
-import { Inicio } from '../components/home/inicio';
-import { Sobre } from '../components/home/sobre';
-import { Solucao } from '../components/home/solucao';
-import { Contato } from '../components/home/contato';
-import { Template } from '../components/utils/template';
+import type { NextPage } from "next";
+import { Inicio } from "../components/home/inicio";
+import { Sobre } from "../components/home/sobre";
+import { Solucao } from "../components/home/solucao";
+import { Contato } from "../components/home/contato";
+import { Template } from "../components/utils/template";
 
 const Home: NextPage = () => {
-    return (
-        <Template>
-            {/* Secctions da landpage */}
-            <Inicio/>
-            <Sobre/>            
-            <Solucao/>            
-            <Contato/>
-        </Template>
-    )
-}
+  return (
+    <Template>
+      <Inicio />
+      <Sobre />
+      <Solucao />
+      <Contato />
+    </Template>
+  );
+};
 
-export default Home
+export default Home;
